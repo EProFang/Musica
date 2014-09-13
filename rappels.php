32484466ae31b985e06a9dbefe98abab67c3c4d6
EProFang <EProFang@hotmail.com>
2014-09-13 07:22:43 -0400

    Rappels PHP

---
+++ rappels.php
@@ -0,0 +1,29 @@
+<?php
+	$carlos 	=50;
+	$maria  	=160;
+	$mama		=100;
+	$papa		=80;
+	
+	$persons = array(
+		'carlos' => $carlos,
+		'maria' => $maria,
+		'mama' => $mama,
+		'papa' => $papa,
+	);
+
+	function add_new_personne($name) {
+			
+		return $name;
+	}
+	function calculate() {
+		foreach( $persons as $key=> $value) {
+			$total = $total + $value;
+		}
+		$total_output .='<div id="total_count">';
+		$total_output .='<p>';
+		$total_output .= 'Total = '. $total .' $';
+		$total_output .='</p>';
+		$total_output .='</div>';
+		echo $total_output;
+	}
+	
